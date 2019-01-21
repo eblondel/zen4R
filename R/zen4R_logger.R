a61a4a45962425fd83164cf5e97986efb8f4d02b
eblondel <emmanuel.blondel1@gmail.com>
2019-01-21 11:12:23 +0100

    fix pkg check issues

--- R/zen4R_logger.R
+++ R/zen4R_logger.R
@@ -1,4 +1,26 @@
+#' zen4RLogger
+#'
+#' @docType class
 #' @export
+#' @keywords logger
+#' @return Object of \code{\link{R6Class}} for modelling a simple logger
+#' @format \code{\link{R6Class}} object.
+#'
+#' @section Abstract Methods:
+#' \describe{
+#'  \item{\code{INFO(text)}}{
+#'    Logger to report information. Used internally
+#'  }
+#'  \item{\code{WARN(text)}}{
+#'    Logger to report warnings. Used internally
+#'  }
+#'  \item{\code{ERROR(text)}}{
+#'    Logger to report errors. Used internally
+#'  }
+#' }
+#' 
+#' @note Logger class used internally by zen4R
+#'
 zen4RLogger <-  R6Class("zen4RLogger",
   public = list(
     #logger
