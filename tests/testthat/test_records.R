c558b3a8e52405674a87a752bed7a56f16602af8
eblondel <emmanuel.blondel1@gmail.com>
2019-05-28 10:49:51 +0200

    record integration tests

--- tests/testthat/test_records.R
+++ tests/testthat/test_records.R
@@ -15,3 +15,32 @@ test_that("create empty record - with pre-reserved DOI",{
   expect_true(nchar(newRec$metadata$prereserve_doi$doi)>0)
   expect_true(newRec$metadata$prereserve_doi$recid>0)
 })
+
+test_that("create and deposit record",{
+  myrec <- ZenodoRecord$new()
+  myrec$setTitle("my R package")
+  myrec$setDescription("A description of my R package")
+  myrec$setUploadType("software")
+  myrec$addCreator(firstname = "John", lastname = "Doe", affiliation = "Independent")
+  myrec$setLicense("mit")
+  myrec$setAccessRight("open")
+  myrec$addCommunity("ecfunded")
+  
+  expect_equal(myrec$metadata$title, "my R package")
+  expect_equal(myrec$metadata$description, "A description of my R package")
+  expect_equal(myrec$metadata$upload_type, "software")
+  expect_is(myrec$metadata$creator, "list")
+  expect_equal(length(myrec$metadata$creator), 1L)
+  expect_equal(myrec$metadata$creator[[1]]$name, "Doe, John")
+  expect_equal(myrec$metadata$creator[[1]]$affiliation, "Independent")
+  
+  expect_true(myrec$metadata$prereserve_doi)
+  
+  #deposit
+  deposit = ZENODO$depositRecord(myrec)
+  expect_is(deposit, "ZenodoRecord")
+  
+  #delete record
+  deleted <- ZENODO$deleteRecord(deposit$id)
+  expect_true(deleted)
+})
