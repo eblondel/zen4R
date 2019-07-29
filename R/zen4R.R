4488055a340700c3340596f94b470fbcd7d35c8f
eblondel <emmanuel.blondel1@gmail.com>
2019-07-29 11:54:26 +0200

    fix description

--- R/zen4R.R
+++ R/zen4R.R
@@ -19,7 +19,7 @@
 #'    Package: \tab zen4R\cr
 #'    Type: \tab Package\cr
 #'    Version: \tab 0.2\cr
-#'    Date: \tab 2019-07-23\cr
+#'    Date: \tab 2019-07-29\cr
 #'    License: \tab MIT\cr
 #'    LazyLoad: \tab yes\cr
 #'  }
