2464d5a653570eacc91b163f6d17e402b1b1df32
eblondel <emmanuel.blondel1@gmail.com>
2019-07-31 10:47:20 +0200

    update pkg date

--- R/zen4R.R
+++ R/zen4R.R
@@ -20,7 +20,7 @@
 #'    Package: \tab zen4R\cr
 #'    Type: \tab Package\cr
 #'    Version: \tab 0.2\cr
-#'    Date: \tab 2019-07-29\cr
+#'    Date: \tab 2019-07-31\cr
 #'    License: \tab MIT\cr
 #'    LazyLoad: \tab yes\cr
 #'  }
