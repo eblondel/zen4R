352b677d4a25bf4f80c4fcfaa238f6b26acb48d8
eblondel <emmanuel.blondel1@gmail.com>
2019-08-21 11:48:42 +0200

    update pkg date

--- R/zen4R.R
+++ R/zen4R.R
@@ -19,8 +19,8 @@
 #'  \tabular{ll}{
 #'    Package: \tab zen4R\cr
 #'    Type: \tab Package\cr
-#'    Version: \tab 0.2\cr
-#'    Date: \tab 2019-08-02\cr
+#'    Version: \tab 0.3\cr
+#'    Date: \tab 2019-08-21\cr
 #'    License: \tab MIT\cr
 #'    LazyLoad: \tab yes\cr
 #'  }
