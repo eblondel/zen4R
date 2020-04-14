d4101f27db83c6b170563cf22ed750334778ea20
eblondel <emmanuel.blondel1@gmail.com>
2020-04-14 17:29:26 +0200

    fix test-all

--- tests/test-all.R
+++ tests/test-all.R
@@ -3,10 +3,10 @@ library(zen4R)
 
 
 #test environment
-zenodo_url <- "https://sandbox.zenodo.org"
+zenodo_url <- "https://sandbox.zenodo.org/api"
 zenodo_token <- "mytoken"
 zenodo_logger <- "DEBUG"
-ZENODO <- try(ZENODO$new(url = zenodo_url, token = zenodo_token, logger = zenodo_logger))
+ZENODO <- try(ZenodoManager$new(url = zenodo_url, token = zenodo_token, logger = zenodo_logger))
 
 if(is(ZENODO, "ZenodoManager")){
   cat(sprintf("Zenodo sandbox '%s' configured with token. Running integration tests...\n", zenodo_url))
