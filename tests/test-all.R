8a0d4e9316c3420c8c6ff40fafe5b3efeff018db
Emmanuel Blondel <emmanuel.blondel1@gmail.com>
2020-04-20 22:17:47 +0200

    Update test-all.R

--- tests/test-all.R
+++ tests/test-all.R
@@ -8,7 +8,8 @@ zenodo_token <- "mytoken"
 zenodo_logger <- "DEBUG"
 ZENODO <- try(ZenodoManager$new(url = zenodo_url, token = zenodo_token, logger = zenodo_logger))
 
-if(is(ZENODO, "ZenodoManager")){
+deps <- ZENODO$getDepositions(size = 1)
+if(is(deps[[1]], "ZenodoRecord")){
   cat(sprintf("Zenodo sandbox '%s' configured with token. Running integration tests...\n", zenodo_url))
   test_check("zen4R")
 }else{
