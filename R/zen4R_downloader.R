dd2ba1413e2868bbbd591299aceed77b8d7bb15d
eblondel <emmanuel.blondel1@gmail.com>
2020-08-07 11:07:29 +0200

    #31 add download_zenodo wrapper

---
+++ R/zen4R_downloader.R
@@ -0,0 +1,30 @@
+#' @name download_zenodo
+#' @aliases download_zenodo
+#' @title download_zenodo
+#' @description \code{download_zenodo} allows to execute a workflow
+#'
+#' @usage download_zenodo("10.5281/zenodo.2547036")
+#'                 
+#' @param doi a Zenodo DOI or concept DOI
+#' @param path the target directory where to download files
+#' @param logger a logger to print messages. The logger can be either NULL, 
+#' "INFO" (with minimum logs), or "DEBUG" (for complete curl http calls logs)
+#' @param ... any other arguments for parallel downloading (more information at
+#'\link{ZenodoManager}, \code{downloadFiles()} documentation)
+#' 
+#' @export
+#' 
+download_zenodo = function(doi, path = ".", logger = NULL, ...){
+  
+  zenodo <- ZenodoManager$new(logger = logger)
+  rec <- zenodo$getRecordByDOI(doi)
+  if(is.null(rec)){
+    #try to get it as concept DOI
+    rec <- zenodo$getRecordByConceptDOI(doi)
+    if(is.null(rec)){
+      stop("The DOI specified doesn't match any existing Zenodo DOI or concept DOI")
+    }
+  }
+  #download
+  rec$downloadFiles(path = path, ...)
+}
