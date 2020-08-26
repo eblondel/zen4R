00d3b4b1a439b5c386a46f050d4f88bd7a96422b
eblondel <emmanuel.blondel1@gmail.com>
2020-08-26 15:34:49 +0200

    contribs + pkg date

--- R/zen4R.R
+++ R/zen4R.R
@@ -23,7 +23,7 @@
 #'    Package: \tab zen4R\cr
 #'    Type: \tab Package\cr
 #'    Version: \tab 0.4\cr
-#'    Date: \tab 2020-08-06\cr
+#'    Date: \tab 2020-08-26\cr
 #'    License: \tab MIT\cr
 #'    LazyLoad: \tab yes\cr
 #'  }
