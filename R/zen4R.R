d9a72d643f468d7695bb87bd80402cc1c29f2b75
florisvdh <floris.vanderhaeghe@inbo.be>
2020-09-03 10:44:28 +0200

    move parallel into suggests, drop import

--- R/zen4R.R
+++ R/zen4R.R
@@ -11,7 +11,6 @@
 #' @import keyring
 #' @importFrom tools file_path_as_absolute
 #' @importFrom tools md5sum
-#' @importFrom parallel mclapply
 #' 
 #' @title Interface to 'Zenodo' REST API
 #' @description Provides an Interface to 'Zenodo' (<https://zenodo.org>) REST API, 
