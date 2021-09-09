d00c76d470a3c2308b6c31a8972f5a465bd83940
eblondel <emmanuel.blondel1@gmail.com>
2021-09-09 09:20:20 +0200

    inspect that secret is well grabed

--- tests/test-all.R
+++ tests/test-all.R
@@ -10,6 +10,7 @@ ZENODO <- try(ZenodoManager$new(url = zenodo_url, token = zenodo_token, logger =
 
 if(is(ZENODO, "ZenodoManager")){
   cat(sprintf("Zenodo sandbox '%s' configured with token. Running integration tests...\n", zenodo_url))
+  cat(sprintf("Zenodo sandbox token = %s", ZENODO$getToken()))
   test_check("zen4R")
 }else{
   cat("Zenodo sandbox '%s' not configured. Skipping integration tests...\n")
