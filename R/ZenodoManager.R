68f46b61f0538e65b8f6096580ebb937459f2eba
eblondel <emmanuel.blondel1@gmail.com>
2021-09-30 14:19:57 +0200

    #57 fix code cleaning

--- R/ZenodoManager.R
+++ R/ZenodoManager.R
@@ -732,7 +732,7 @@ ZenodoManager <-  R6Class("ZenodoManager",
       if(zenReq$getStatus() %in% c(200,201)){
         out <- zenReq$getResponse()
         out_id <- unlist(strsplit(out$links$latest_draft,"depositions/"))[[2]]
-        out <-  ZENODO$getDepositionById(out_id)
+        out <-  self$getDepositionById(out_id)
         self$INFO(sprintf("Successful new version record created for concept DOI '%s'", record$getConceptDOI()))
         record$id <- out$id
         record$metadata$doi <- NULL
