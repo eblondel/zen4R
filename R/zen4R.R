4d52abbbf3a8cff75421cb230256f5e075e21e8b
eblondel <emmanuel.blondel1@gmail.com>
2021-11-24 23:16:16 +0100

    pending rev 0.5-2

--- R/zen4R.R
+++ R/zen4R.R
@@ -21,8 +21,8 @@
 #'  \tabular{ll}{
 #'    Package: \tab zen4R\cr
 #'    Type: \tab Package\cr
-#'    Version: \tab 0.5-1\cr
-#'    Date: \tab 2021-10-29\cr
+#'    Version: \tab 0.5-2\cr
+#'    Date: \tab 2021-11-24\cr
 #'    License: \tab MIT\cr
 #'    LazyLoad: \tab yes\cr
 #'  }
