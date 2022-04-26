7320f3c89733c2752821e49bf3686913fefa4b41
eblondel <emmanuel.blondel1@gmail.com>
2022-04-26 17:10:23 +0200

    fix #72 sandbox attribute

--- R/ZenodoManager.R
+++ R/ZenodoManager.R
@@ -446,7 +446,7 @@ ZenodoManager <-  R6Class("ZenodoManager",
                               quiet = FALSE){
       page <- 1
       baseUrl <- "deposit/depositions"
-      if(!sandbox) baseUrl <- paste0(baseUrl, "/")
+      if(!private$sandbox) baseUrl <- paste0(baseUrl, "/")
       req <- sprintf("%s?q=%s&size=%s&page=%s", baseUrl, URLencode(q), size, page)
       if(all_versions) req <- paste0(req, "&all_versions=1")
       zenReq <- ZenodoRequest$new(private$url, "GET", req, 
